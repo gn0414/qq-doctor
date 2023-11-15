21366c9ad216e1b99f18e8e3c14d8ffa72d971ec
simon <815175473@qq.com>
2023-11-15 17:41:00 +0800

    服务发现与配置完成

--- user/src/main/java/com/qiqiao/user/UserApplication.java
+++ user/src/main/java/com/qiqiao/user/UserApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * @author Simon
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class UserApplication {
