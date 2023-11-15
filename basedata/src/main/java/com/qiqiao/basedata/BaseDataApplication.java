381c9ed81e1ce35b6f35bd6dba6c90760f9944d7
simon <815175473@qq.com>
2023-11-15 18:59:09 +0800

    知识库模块添加

---
+++ basedata/src/main/java/com/qiqiao/basedata/BaseDataApplication.java
@@ -0,0 +1,18 @@
+package com.qiqiao.basedata;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Simon
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BaseDataApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaseDataApplication.class,args);
+    }
+}
